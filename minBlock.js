4616057f1084ca99613d16e9d093ff52d7309350
GreenLantern101 <arctangent20@yahoo.com>
2016-11-22 10:28:51 -0600

    major refactoring, commenting, optimization
    - DRY principle
    - global vars --> will fix in later commit
    - initial buggy support for NxN matrix
    - optimized enlight() function

--- minBlock.js
+++ minBlock.js
@@ -4,109 +4,59 @@
  * Under MIT Lisence
  * Contributors : Alvaro Pinot @alvaropinot http://github.com/alvaropinot
  */
-function putSquare(ctx, x, y, height, width, color) {
+
+var blockStack = 5;
+var Height, Width, squareHeight, squareWidth, padding, ctx;
+
+function putSquare(x, y, color) {
 
     ctx.beginPath();
     ctx.fillStyle = color;
-    ctx.rect(x, y, width, height);
+    ctx.rect(x, y, squareWidth, squareHeight);
     ctx.fill();
     ctx.closePath();
 }
 
 
-function populateMatrix(ctx, height, width, padding, blockStack, color) {
-
-    var Height = height - 2 * padding;
-    var Width = width - 2 * padding;
-
-    var squareHeight = Height / blockStack;
-    var squareWidth = Height / blockStack;
+function fillMatrix(color) {
 
     for (var i = 0; i < blockStack; i++) {
-
         for (var j = 0; j < blockStack; j++) {
-            putSquare(ctx, padding + (j * squareWidth), padding + (i * squareHeight), squareHeight, squareWidth, color);
+            enlight(color, j, i);
         }
     }
-
 }
 
-
-function enlight(ctx, height, width, padding, blockStack, color, matrixX, matrixY) {
-
-    var Height = height - 2 * padding;
-    var Width = width - 2 * padding;
-
-    var squareHeight = Height / blockStack;
-    var squareWidth = Height / blockStack
-
-
-    for (var i = 0; i < blockStack; i++) {
-
-        for (var j = 0; j < blockStack; j++) {
-
-            if ((i == matrixY) && (j == matrixX)) {
-                putSquare(ctx, padding + (j * squareWidth), padding + (i * squareHeight), squareHeight, squareWidth, color);
-            }
-        }
-    }
-
-
+function enlight(color, matrixX, matrixY) {
+    putSquare(padding + (matrixX * squareWidth),
+        padding + (matrixY * squareHeight), color);
 }
 
 
 function getLocation(number) {
-
-
-    if (number == 0)
-        return [0, 0];
-    if (number == 1)
-        return [0, 1];
-    if (number == 2)
-        return [0, 2];
-
-
-    if (number == 3)
-        return [1, 0];
-    if (number == 4)
-        return [1, 1];
-    if (number == 5)
-        return [1, 2];
-
-
-    if (number == 6)
-        return [2, 0];
-    if (number == 7)
-        return [2, 1];
-    if (number == 8)
-        return [2, 2];
+    var a = Math.floor(number / blockStack);
+    var b = number % blockStack;
+    return [a, b];
 }
 
 
+function splitnFill(color, number) {
 
-function fillIt(ctx, height, width, padding, blockStack, color, number) {
-
-    var coordinate = getLocation(number);
-    enlight(ctx, height, width, padding, blockStack, color, coordinate[1], coordinate[0]);
-}
-
-
-function splitnFill(ctx, height, width, padding, blockStack, color, number) {
-
-    var ob = String(number).split('').map(Number);
+    //converts number into an array of its digits
+    var digits = String(number).split('').map(Number);
     var i = 0;
-    while (i < ob.length) {
-        fillIt(ctx, height, width, padding, blockStack, color, ob[i]);
+
+    //for each digit...
+    while (i < digits.length) {
+        var coordinate = getLocation(digits[i]);
+        enlight(color, coordinate[1], coordinate[0]);
         i++;
     }
 }
 
-
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-
 
 //var timestamp = new Date().getUTCMilliseconds();
 function init(config) {
@@ -121,7 +71,7 @@ function init(config) {
         pause : false
     }*/
 
-    if(!config.divId) {
+    if (!config.divId) {
         return;
     }
 
@@ -131,8 +81,10 @@ function init(config) {
     if (!config.pause) {
         config.pause = false;
     }
-    var buffer = new Date().getUTCMilliseconds();
     var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'];
+
+    //TODO: make light/dark colors? contrasting combo
+
 
     if (!config.randomColor) {
         var colorPrimary = colors[Math.floor(Math.random() * colors.length)];
@@ -146,32 +98,32 @@ function init(config) {
     }
     var canvas = document.getElementById(config.divId);
     canvas.style.background = colorPrimary;
-    var ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d');
     var canvasWidth = canvas.width;
     var canvasHeight = canvas.height;
-    var padding = 20;
-    var blockStack = 3;
-    var buffer
-    populateMatrix(ctx, canvasWidth, canvasHeight, padding, blockStack, colorSecondary);
+
+    padding = 20;
+    //initialize height/width vars ----------------
+    Height = canvasHeight - 2 * padding;
+    Width = canvasWidth - 2 * padding;
+    //number of squares vertically and horizontally
+    squareHeight = Height / blockStack;
+    squareWidth = Width / blockStack;
+    //--------------------------------------------
+
+    fillMatrix(colorSecondary);
 
     if (!config.pause) {
         setInterval(function() {
-
-            //colorPrimary = colors[Math.floor(Math.random() * colors.length)];
-            //colorSecondary = colors[Math.floor(Math.random() * colors.length)];
-            //while( colorSecondary == colorPrimary){
-            //colorSecondary = colors[Math.floor(Math.random() * colors.length)];
-            //}
-            var timestamp = getRandomInt(0, 900);
-            populateMatrix(ctx, canvasWidth, canvasHeight, padding, blockStack, colorSecondary);
-            splitnFill(ctx, canvasWidth, canvasHeight, padding, blockStack, colorPrimary, timestamp);
-
+            refresh();
         }, config.time);
     } else {
+        refresh();
+    }
 
+    function refresh() {
         var timestamp = getRandomInt(0, 900);
-        populateMatrix(ctx, canvasWidth, canvasHeight, padding, blockStack, colorSecondary);
-        splitnFill(ctx, canvasWidth, canvasHeight, padding, blockStack, colorPrimary, timestamp);
-
+        fillMatrix(colorSecondary);
+        splitnFill(colorPrimary, timestamp);
     }
 }
