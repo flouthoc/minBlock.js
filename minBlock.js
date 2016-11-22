29e554f654f5039dade3dc2f3e132bed6945c6bc
GreenLantern101 <arctangent20@yahoo.com>
2016-11-22 11:02:03 -0600

    wrapped minBlock as an object, no more global vars

--- minBlock.js
+++ minBlock.js
@@ -5,125 +5,135 @@
  * Contributors : Alvaro Pinot @alvaropinot http://github.com/alvaropinot
  */
 
-var blockStack = 5;
-var Height, Width, squareHeight, squareWidth, padding, ctx;
+var minBlock = function(config) {
+    var blockStack = 5;
+    var Height,
+        Width,
+        squareHeight,
+        squareWidth,
+        padding,
+        ctx;
 
-function putSquare(x, y, color) {
+    function init(config) {
 
-    ctx.beginPath();
-    ctx.fillStyle = color;
-    ctx.rect(x, y, squareWidth, squareHeight);
-    ctx.fill();
-    ctx.closePath();
-}
+        config = config || {};
+        /*defaults = {
+            divId :
+            time :
+            randomColor : false,
+            colorPrimary :
+            colorSecondary :
+            pause : false
+        }*/
 
-
-function fillMatrix(color) {
-
-    for (var i = 0; i < blockStack; i++) {
-        for (var j = 0; j < blockStack; j++) {
-            enlight(color, j, i);
+        if (!config.divId) {
+            return;
         }
-    }
-}
 
-function enlight(color, matrixX, matrixY) {
-    putSquare(padding + (matrixX * squareWidth),
-        padding + (matrixY * squareHeight), color);
-}
-
-
-function getLocation(number) {
-    var a = Math.floor(number / blockStack);
-    var b = number % blockStack;
-    return [a, b];
-}
-
-
-function splitnFill(color, number) {
-
-    //converts number into an array of its digits
-    var digits = String(number).split('').map(Number);
-    var i = 0;
-
-    //for each digit...
-    while (i < digits.length) {
-        var coordinate = getLocation(digits[i]);
-        enlight(color, coordinate[1], coordinate[0]);
-        i++;
-    }
-}
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-//var timestamp = new Date().getUTCMilliseconds();
-function init(config) {
-
-    config = config || {};
-    /*defaults = {
-        divId :
-        time :
-        randomColor : false,
-        colorPrimary :
-        colorSecondary :
-        pause : false
-    }*/
-
-    if (!config.divId) {
-        return;
-    }
-
-    if (!config.randomColor) {
-        config.randomColor = false;
-    }
-    if (!config.pause) {
-        config.pause = false;
-    }
-    var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'];
-
-    //TODO: make light/dark colors? contrasting combo
-
-
-    if (!config.randomColor) {
-        var colorPrimary = colors[Math.floor(Math.random() * colors.length)];
-        var colorSecondary = colors[Math.floor(Math.random() * colors.length)];
-        while (colorSecondary == colorPrimary) {
-            colorSecondary = colors[Math.floor(Math.random() * colors.length)];
+        if (!config.randomColor) {
+            config.randomColor = false;
         }
-    } else {
-        var colorPrimary = config.colorPrimary;
-        var colorSecondary = config.colorSecondary;
-    }
-    var canvas = document.getElementById(config.divId);
-    canvas.style.background = colorPrimary;
-    ctx = canvas.getContext('2d');
-    var canvasWidth = canvas.width;
-    var canvasHeight = canvas.height;
+        if (!config.pause) {
+            config.pause = false;
+        }
+        var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'];
 
-    padding = 20;
-    //initialize height/width vars ----------------
-    Height = canvasHeight - 2 * padding;
-    Width = canvasWidth - 2 * padding;
-    //number of squares vertically and horizontally
-    squareHeight = Height / blockStack;
-    squareWidth = Width / blockStack;
-    //--------------------------------------------
+        //TODO: make light/dark colors? contrasting combo
 
-    fillMatrix(colorSecondary);
 
-    if (!config.pause) {
-        setInterval(function() {
-            refresh();
-        }, config.time);
-    } else {
-        refresh();
-    }
+        if (!config.randomColor) {
+            var colorPrimary = colors[Math.floor(Math.random() * colors.length)];
+            var colorSecondary = colors[Math.floor(Math.random() * colors.length)];
+            while (colorSecondary == colorPrimary) {
+                colorSecondary = colors[Math.floor(Math.random() * colors.length)];
+            }
+        } else {
+            var colorPrimary = config.colorPrimary;
+            var colorSecondary = config.colorSecondary;
+        }
 
-    function refresh() {
-        var timestamp = getRandomInt(0, 900);
+        var canvas = document.getElementById(config.divId);
+        canvas.style.background = colorPrimary;
+        var canvasWidth = canvas.width;
+        var canvasHeight = canvas.height;
+
+
+        ctx = canvas.getContext('2d');
+        padding = 20;
+        //initialize height/width vars ----------------
+        Height = canvasHeight - 2 * padding;
+        Width = canvasWidth - 2 * padding;
+        //number of squares vertically and horizontally
+        squareHeight = Height / blockStack;
+        squareWidth = Width / blockStack;
+        //--------------------------------------------
+
         fillMatrix(colorSecondary);
-        splitnFill(colorPrimary, timestamp);
+
+        if (!config.pause) {
+            setInterval(function() {
+                refresh();
+            }, config.time);
+        } else {
+            refresh();
+        }
+
+        function refresh() {
+            var timestamp = getRandomInt(0, 900);
+            fillMatrix(colorSecondary);
+            splitnFill(colorPrimary, timestamp);
+        }
+    }
+
+    init(config);
+
+
+    function putSquare(x, y, color) {
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.rect(x, y, squareWidth, squareHeight);
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    //fills entire matrix a single color
+    function fillMatrix(color) {
+        for (var i = 0; i < blockStack; i++) {
+            for (var j = 0; j < blockStack; j++) {
+                enlight(color, j, i);
+            }
+        }
+    }
+
+    //lights a single square a color
+    function enlight(color, matrixX, matrixY) {
+        putSquare(padding + (matrixX * squareWidth),
+            padding + (matrixY * squareHeight), color);
+    }
+
+
+    function getLocation(number) {
+        var a = Math.floor(number / blockStack);
+        var b = number % blockStack;
+        return [a, b];
+    }
+
+
+    function splitnFill(color, number) {
+
+        //converts number into an array of its digits
+        var digits = String(number).split('').map(Number);
+        var i = 0;
+
+        //for each digit...
+        while (i < digits.length) {
+            var coordinate = getLocation(digits[i]);
+            enlight(color, coordinate[1], coordinate[0]);
+            i++;
+        }
+    }
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
