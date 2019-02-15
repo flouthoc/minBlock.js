f075f76b1c2e14bf23d3b7bb519abd2863dea142
Alex Lübeck <ndsvw@gmx.de>
2019-02-15 08:29:38 +0100

    improved the parameters of the config object, only canvasID is required now, making the padding value settable, rendering it only one time is the default now, added more comments

--- minBlock.js
+++ minBlock.js
@@ -5,75 +5,55 @@
  * Contributors :
  * Alvaro Pinot @alvaropinot http://github.com/alvaropinot
  * GreenLantern101 https://github.com/GreenLantern101
+ * ndsvw https://github.com/ndsvw
  */
 
-var minBlock = function(config) {
+var minBlock = function (config) {
 
-    if (!config.divId) {
+    //TODO: make light/dark colors? contrasting combo
+    var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'];
+
+    var options = {};
+
+    if (typeof config !== "object" || !config.canvasID) {
         return;
     }
 
-    //private vars
-    var blockStack = config.numBlocks;
-    //number of blocks per edge must be at least three
-    blockStack = Math.max(blockStack, 3);
+    options.canvasID = config.canvasID;
+    options.blocksPerEdge = Math.max(config.blocksPerEdge || 5, 3);
+    options.padding = (typeof config.padding === "number") ? config.padding : 20;
+    options.color = (typeof options.color === "object") ? config.color : getRandomColorPair();
 
-    var canvas = document.getElementById(config.divId);
+    var canvas = document.getElementById(options.canvasID);
     var canvasWidth = canvas.width;
     var canvasHeight = canvas.height;
     var ctx = canvas.getContext('2d');
-    var padding = 20;
 
-    var Height = canvasHeight - 2 * padding;
-    var Width = canvasWidth - 2 * padding;
-    //number of squares vertically and horizontally
-    var squareHeight = Height / blockStack;
-    var squareWidth = Width / blockStack;
+    var Height = canvasHeight - 2 * options.padding;
+    var Width = canvasWidth - 2 * options.padding;
+    var squareHeight = Height / options.blocksPerEdge;
+    var squareWidth = Width / options.blocksPerEdge;
 
-    var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c'];
-
-    //TODO: make light/dark colors? contrasting combo
-
-
-    if (!config.randomColor) {
-        var colorPrimary = colors[Math.floor(Math.random() * colors.length)];
-        var colorSecondary = colors[Math.floor(Math.random() * colors.length)];
-        while (colorSecondary == colorPrimary) {
-            colorSecondary = colors[Math.floor(Math.random() * colors.length)];
-        }
-    } else {
-        var colorPrimary = config.colorPrimary;
-        var colorSecondary = config.colorSecondary;
+    //render
+    function render() {
+        canvas.style.background = options.color.primary;
+        fillMatrix(options.color.secondary);
+        randFill(options.color.primary);
     }
 
-    //accounts for border padding
-    canvas.style.background = colorPrimary;
-
-    if (!config.pause) {
-        setInterval(function() {
-            refresh();
-        }, config.time);
-    } else {
-        refresh();
-    }
-    //generate a new random matrix
-    function refresh() {
-        var timestamp = getRandomInt(0, 900);
-        fillMatrix(colorSecondary);
-        randFill(colorPrimary, timestamp);
-    }
     //fills entire matrix a single color
     function fillMatrix(color) {
-        for (var i = 0; i < blockStack; i++) {
-            for (var j = 0; j < blockStack; j++) {
+        for (var i = 0; i < options.blocksPerEdge; i++) {
+            for (var j = 0; j < options.blocksPerEdge; j++) {
                 drawSquare(color, j, i);
             }
         }
     }
+
     //draws a single color square
     function drawSquare(color, matrixX, matrixY) {
-        var x = padding + (matrixX * squareWidth);
-        var y = padding + (matrixY * squareHeight);
+        var x = options.padding + (matrixX * squareWidth);
+        var y = options.padding + (matrixY * squareHeight);
         ctx.beginPath();
         ctx.fillStyle = color;
         ctx.rect(x, y, squareWidth, squareHeight);
@@ -81,26 +61,38 @@ var minBlock = function(config) {
         ctx.closePath();
     }
 
+    //get a random color pair
+    function getRandomColorPair() {
+        var colorPrimary = colors[Math.floor(Math.random() * colors.length)];
+        var colorSecondary = colors[Math.floor(Math.random() * colors.length)];
+        while (colorSecondary === colorPrimary) {
+            colorSecondary = colors[Math.floor(Math.random() * colors.length)];
+        }
+        return { primary: colorPrimary, secondary: colorSecondary };
+    }
+
+    //get the location dependant on a number
     function getLocation(number) {
-        var a = Math.floor(number / blockStack);
-        var b = number % blockStack;
+        var a = Math.floor(number / options.blocksPerEdge);
+        var b = number % options.blocksPerEdge;
         return [a, b];
     }
 
-    function randFill(color, number) {
-        //converts number into an array of its digits
-        //var digits = String(number).split('').map(Number);
-        var numBlocksReplace = getRandomInt(blockStack * blockStack * .25, blockStack * blockStack * .75);
+    //fill the canvas with blocks at random positions
+    function randFill(color) {
+        var numBlocksReplace = getRandomInt(options.blocksPerEdge * options.blocksPerEdge * .25, options.blocksPerEdge * options.blocksPerEdge * .75);
         for (var i = 0; i < numBlocksReplace; i++) {
-            var coordinate = getLocation(getRandomInt(0, blockStack * blockStack - 1));
+            var coordinate = getLocation(getRandomInt(0, options.blocksPerEdge * options.blocksPerEdge - 1));
             drawSquare(color, coordinate[1], coordinate[0]);
         }
     }
 
+    //get a random integer between min and max
     function getRandomInt(min, max) {
-        //force integers
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    render()
 }
